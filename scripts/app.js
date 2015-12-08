53802a7c20b018e98a16f4138f9d5f96bcaae72b
carlsim <similatron@gmail.com>
2015-12-07 23:00:14 -0500

    major restructuring of app'

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,20 @@
-var app = angular.module('GalleryApp', ['ngRoute']);
+var galleryApp = angular.module('galleryApp', [
+	'ngRoute',
+	'galleryControllers'
+	]);
 
-app.config(function($routeProvider){
+galleryApp.config(['$routeProvider', 
+	function($routeProvider){
 	$routeProvider
 	.when('/',{
-		controller: 'MainController',
+		controller: 'GalleryListCtrl',
 		templateUrl: 'views/home.html'
 	})
-	.when('/photos/:id',{
-		controller: 'PhotosController',
+	.when('/photos/:photoId',{
+		controller: 'GalleryDetailCtrl',
 		templateUrl: 'views/photoview.html'
 	})
 	.otherwise({
 		redirectTo: '/'
 	});
-});
+}]);
